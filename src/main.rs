d48a83eea5f9e6abddc67e2090b8d5b99267a10c
Ruleofnine <ruleofnine@outlook.com>
2023-09-27 20:45:05 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+
+use dotenv::dotenv;
+use std::env;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use log::info;
+struct Handler;
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if !msg.author.bot {
+        info!("{}: {}",msg.author.name,msg.content);
+        }
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info,serenity=error,tracing=error"));
+    let token = env::var("BOT_TOKEN").expect("BOT_TOKEN not found");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT| GatewayIntents::GUILDS;
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
